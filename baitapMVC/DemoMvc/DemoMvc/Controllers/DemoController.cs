e20a7a598413efdcc5bf60c8a51ae06893dbaeb8
Huy-Loc <huyloc120199@gmail.com>
2021-08-24 14:20:15 +0700

    1

---
+++ baitapMVC/DemoMvc/DemoMvc/Controllers/DemoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMvc.Models;
+
+namespace DemoMvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hello()
+        {
+            return View();
+        }
+        public ActionResult GiaiPhuongTrinhB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPhuongTrinhB1(double SoA,double SoB)
+        {
+
+            Double x = gpt.GiaiPhuongTrinhBac1(SoA, SoB);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+
+    }
+}
---
+++ baitapMVC/DemoMvc/DemoMvc/Models/GiaiPhuongTrinh.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMvc.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public double GiaiPHuongTrinhBacNhat(double a, double b)
+        {
+            double x;
+            x = -b / a; 
+            return x;
+            
+        }
+    }
+}
