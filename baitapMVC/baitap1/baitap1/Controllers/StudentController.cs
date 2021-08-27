6eaf771975a968f0db039cdec6c1febeb24af43c
Huy-Loc <huyloc120199@gmail.com>
2021-08-27 13:06:39 +0700

    1

---
+++ baitapMVC/baitap1/baitap1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using class1baitap1.Models;
+
+namespace baitap1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ baitapMVC/baitap1/baitap1/Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap1.Models
+{
+    public class Class1
+    {
+        public string TTSV()
+        {
+            String ten, lop;
+            int tuoi;
+
+            return x;
+        }
+    }
+}
